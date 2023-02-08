e5339116719f124c40a3fed0b7c592bc15180b79
arifozel <89255200+arifozel@users.noreply.github.com>
2023-02-09 01:39:11 +0300

    Add files via upload

---
+++ etkinlik/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Hava Derecesini Giriniz : ");
+        heat = inp.nextInt();
+
+        if (heat < 5) {
+            System.out.println("Kayak yapabilirsiniz");
+
+        } else if (heat <= 25) {
+            if (heat <= 15) {
+                System.out.println("Sinemaya Gidebilirsiniz");
+
+            }
+            if (heat >= 10) {
+                System.out.println("Pikniğe Gidebilirsiniz");
+
+            } else {
+                System.out.println("Yüzmeye Gidebilirsiniz");
+
+
+                }
+
+            }
+        }
+    }
